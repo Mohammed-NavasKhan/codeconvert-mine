83a309c14a3280e338760d1d7641df603a65b731
Mohammed-NavasKhan <programwithnavas@gmail.com>
2025-04-16 14:53:29 +0530

    added position

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const SidebarContext = createContext();
 export default function Sidebar() {
   const [expanded, setExpanded] = useState(true);
   return (
-    <div className="flex fixed">
+    <div className="flex fixed z-10">
       <aside className="h-screen">
         <nav className="h-full flex flex-col bg-white border-r shadow-sm">
           <div className="p-4 pb-2 flex justify-between items-center">
