b69e52dfc3624dfb73675ff8e7ee8dec3db846d5
Mohammed-NavasKhan <programwithnavas@gmail.com>
2025-04-16 14:41:48 +0530

    added sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Breadcrumb from './components/Breadcrumb';
-import Home from './pages/Home';
-import CodeConverter from './pages/CodeConverter';
-import Mortgages from './pages/Mortgages'
-import Chat from './pages/Chat';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Breadcrumb from "./components/Breadcrumb";
+import Homepage from "./pages/Home";
+import CodeConverter from "./pages/CodeConverter";
+import Mortgages from "./pages/Mortgages";
+import Chat from "./pages/Chat";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         {/* <Navbar />
         <Breadcrumb /> */}
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Chat />} />
           <Route path="/modernisation" element={<CodeConverter />} />
           <Route path="/mortgages" element={<Mortgages />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Homepage />} />
         </Routes>
       </div>
     </Router>
